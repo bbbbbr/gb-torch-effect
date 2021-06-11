2ffae0431038cb744ac7f3fe761f86b9670a6ef6
bbbbbr <bbbbbr@users.noreply.github.com>
2021-06-11 13:18:00 -0700

    Seems to be working now. Mostly sorted out timing issues.
    Since end of Mode 3 -> 0 timing can fluctuate with extra cycles when sprites are fetched, use the (hoepfully) more consistant mode 0 -> 2 transition to align instead

--- src/lcd_isr.s
+++ src/lcd_isr.s
@@ -37,7 +37,7 @@
 ;	and a, #0x07
 ;	ld  hl, #jump_offset$
 ;wait_scx_offset$:
-;	add a, (hl)
+;	sub a, (hl)
 ;	jp 	(hl)
 
 ;	nop
@@ -52,37 +52,46 @@
 ;jump_offset$:
 
 
-; ----------------- Prep for WX Right-side Rounded Window Update
-; WX_REG = *p_x_end;
-;	ld	hl, #_p_x_end
+								; == Prep for WX Right-side Rounded Window Update
+								; Loads address at *p_x_end in BC
 	ld	hl, #_p_x_end+0
 	ld	c, (hl)
 	inc hl
 	ld	b, (hl)
 	ld	a, (bc)
-;	ldh	(_WX_REG+0),a   ; This is performed below each scanline + dec c
+								
+	ldh	(_WX_REG+0), a			; == Load Window location for first line
+	inc	c        				; Move pointer to next LUT entry (LUT is 128 byte aligned, don't need a 16 bit add
 
-	; Load Window location for first line
-;	ld	a, (bc)  		; Load WX for current scanline from LUT
-	ldh	(_WX_REG+0), a
-	; p_x_end++;
-	inc	c        		; Move pointer to next LUT entry (LUT is 128 byte aligned, don't need a 16 bit add
+								; Then This is performed below for each scanline + inc c
+								; 	ld	a, (bc)
+								;	ldh	(_WX_REG+0),a   
+								; 	inc	c
 
 
+								; Try to line main loop up with start of Mode 2 (Search Objs)
+
+								; Per-Scanline Modes: 2 (Search Objs) -> 3 (Render) -> 0 (H-Blank)
+								;
+								; == Since end of Mode 3 -> 0 timing can fluctuate with
+								;    extra cycles when sprites are fetched, use the (hoepfully)
+								;    more consistant mode 0 -> 2 transition to align instead
+	ld h, #0x03 
+wait_mode_10$:					; Wait for H-Blank (Mode 0)
+    ldh	a, (_STAT_REG+0)  ; 3
+;	bit	1, a
+;	and a, #0x03
+	and a, h
+	jr	nz, wait_mode_10$		; Exit loop on Mode 3 (Render) -> 0 (H-Blank) transition
 
 wait_mode_23$:
     ldh	a, (_STAT_REG+0)
-	bit	1, a
-	jr	z, wait_mode_23$		; Wait until mode 2/3   <-- optimize this (wait till mode 2 at least)
-
-wait_mode_10$:
-    ldh	a, (_STAT_REG+0)  ; 3
-	bit	1, a
-;	and a, #0x03
-	jr	nz, wait_mode_10$		; On first pass -> Wait until mode 01
-								; Try to exit the loop on the mode 3 -> 0 transition
+;	bit	1, a
+	and a, h	
+	jr	z, wait_mode_23$		; Exit loop on Mode 0 (H-Blank) -> 2 (Search Objs) transition
 
 
+								; Should now be ~ Mode 2 (Search Objs)
 lcd_loop_start$:
 
 ;;	ld		a, #0x00            ; White
@@ -102,71 +111,9 @@ lcd_loop_start$:
 	nop
 	nop
 	nop
-	nop
+	nop						; ~ Mode 3 (Render) begins
 
 	nop
-	nop
-	nop
-	nop
-	nop
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-
-    nop
-    nop
-    nop
-    nop
-    nop
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-;	ld		a, #0xEF
-;	ldh		(_LCDC_REG+0),a ; Swap BG Tile Map to Alt
-
-
-
-	nop
-	nop
-	nop
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-
-
-	nop
-	nop
-	nop
-	nop
-	nop
-
-
-
 	nop
 	nop
 	nop
@@ -174,32 +121,12 @@ lcd_loop_start$:
 
 	ld		a, #0xE7
 	ldh		(_LCDC_REG+0),a ; Swap BG Tile Map to Main
-
-	nop
-
 ;	; Sort of ok, sort of not
 ;	ld		a, #0xE7
 ;	ldh		(_LCDC_REG+0),a ; Swap BG Tile Map to Main
 ;;	ld		a, #0xE4
 ;;	ldh		(_BGP_REG+0),a		; Set Background Pal to Normal
 
-
-	nop
-	nop
-
-	; Sort of ok, sort of not
-;	ld		a, #0xE7
-;	ldh		(_LCDC_REG+0),a ; Swap BG Tile Map to Main
-
-	; not quite far enough left
-;	ld		a, #0xE4
-;	ldh		(_BGP_REG+0),a		; Set Background Pal to Normal
-
-	nop
-	nop
-	nop
-
-
 	nop
 	nop
 	nop
@@ -212,6 +139,50 @@ lcd_loop_start$:
 	nop
 	nop
 
+    nop
+    nop
+    nop
+    nop
+    nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+
+
+	nop
+
+
 
 	nop
 	nop
@@ -222,16 +193,9 @@ lcd_loop_start$:
 
 	nop
 	nop
+	nop						; ~ Mode 0 (HBlank) begins
 	nop
 	nop
-	nop
-
-							; == Test for Exit based on LY
-	ldh	a, (_LY_REG+0)
-	ld	hl, #_y_line_end
-	sub	a, (hl)
-	jp 	Z, lcd_isr_exit$	; Exit after ending Y line reached
-
 
 							; == WX Right-side Rounded Window Update
 							;
@@ -242,6 +206,35 @@ lcd_loop_start$:
 	ld	a, (bc)  			; Load WX for current scanline from LUT
 	ldh	(_WX_REG+0), a
 	inc	c        			; Move pointer to next LUT entry (LUT is 128 byte aligned, don't need a 16 bit add
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+
+							; == Test for Exit based on LY
+	ldh	a, (_LY_REG+0)
+	ld	hl, #_y_line_end
+	sub	a, (hl)
+	jp 	Z, lcd_isr_exit$	; Exit after ending Y line reached
+
+
 
 ; -----------------
 
