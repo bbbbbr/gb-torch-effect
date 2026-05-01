003265e6edcca11c9a32bb091d0e1c2c6bb25403
bbbbbr <bbbbbr@users.noreply.github.com>
2026-05-01 01:07:58 -0700

    Coffee-power-up method: fix up hardware tilemap wraparound step down and occasional cleanup glitch on scroll wraparound. - Also support diagonal movement

--- coffee-power-up-method/src/main.c
+++ coffee-power-up-method/src/main.c
@@ -8,6 +8,10 @@
 
 #include "common.h"
 
+// Clamps to hardware tilemap width and height in tiles
+// Avoids Y step down when wrapping around > 31
+#define TM_CLAMP_W(n) (n & ((uint8_t)DEVICE_SCREEN_BUFFER_WIDTH - 1u))
+#define TM_CLAMP_H(n) (n & ((uint8_t)DEVICE_SCREEN_BUFFER_HEIGHT - 1u))
 
 // ====================================================================
 
@@ -56,13 +60,13 @@ static uint8_t atr_cent_x_last = 0u;
 static uint8_t atr_cent_y_last = 0u;
 
 void redraw_attribute_map_torch_effect(void) {
-       
+
     uint8_t atr_cent_x = ((SPR_PLY_X - DEVICE_SPRITE_PX_OFFSET_X) + SCX_REG) >> 3;
     uint8_t atr_cent_y = ((SPR_PLY_Y - DEVICE_SPRITE_PX_OFFSET_Y) + SCY_REG) >> 3;
 
     VBK_REG = VBK_ATTRIBUTES;
     // Draw edges
-    set_bkg_tiles(atr_cent_x - TORCH_WIDTH_ST, atr_cent_y - TORCH_HEIGHT_ST, 
+    set_bkg_tiles(atr_cent_x - TORCH_WIDTH_ST, atr_cent_y - TORCH_HEIGHT_ST,
                   TORCH_WIDTH, TORCH_HEIGHT, torch_attrib_overlay_map);
     // fill_bkg_rect(atr_cent_x - 1, atr_cent_y - 2, 3, 1, PAL_MED);
     // fill_bkg_rect(atr_cent_x - 1, atr_cent_y + 2, 3, 1, PAL_MED);
@@ -83,14 +87,14 @@ void redraw_attribute_map_torch_effect(void) {
     // set_bkg_tile_xy(atr_cent_x    , atr_cent_y + 3, PAL_MED);
 
     VBK_REG = VBK_TILES;
-    
+
     atr_cent_x_last = atr_cent_x;
     atr_cent_y_last = atr_cent_y;
 }
 
 
-void update_attribute_map_torch_effect(void) {
-    
+void update_attribute_map_torch_effect(uint8_t move_dir) {
+
     uint8_t atr_cent_x = ((SPR_PLY_X - DEVICE_SPRITE_PX_OFFSET_X) + SCX_REG) >> 3;
     uint8_t atr_cent_y = ((SPR_PLY_Y - DEVICE_SPRITE_PX_OFFSET_Y) + SCY_REG) >> 3;
 
@@ -98,51 +102,57 @@ void update_attribute_map_torch_effect(void) {
 
         VBK_REG = VBK_ATTRIBUTES;
         // Erase Edge of old position
-        if (atr_cent_x > atr_cent_x_last) { // Moving Right
-            fill_bkg_rect(atr_cent_x_last - TORCH_WIDTH_ST, atr_cent_y_last - TORCH_WIDTH_ST, 1, TORCH_HEIGHT, PAL_DRK);
+
+        if (move_dir & J_LEFT) {
+            fill_bkg_rect(TM_CLAMP_W(atr_cent_x_last + TORCH_WIDTH_ST),
+                          TM_CLAMP_H(atr_cent_y_last - TORCH_HEIGHT_ST), 1, TORCH_HEIGHT, PAL_DRK);
         }
-        else if (atr_cent_x < atr_cent_x_last) { // Moving Left
-            fill_bkg_rect(atr_cent_x_last + TORCH_WIDTH_ST, atr_cent_y_last - TORCH_WIDTH_ST, 1, TORCH_HEIGHT, PAL_DRK);
+        else if (move_dir & J_RIGHT) {
+            fill_bkg_rect(TM_CLAMP_W(atr_cent_x_last - TORCH_WIDTH_ST),
+                          TM_CLAMP_H(atr_cent_y_last - TORCH_HEIGHT_ST), 1, TORCH_HEIGHT, PAL_DRK);
         }
 
-        if (atr_cent_y > atr_cent_y_last) { // Moving Down
-            fill_bkg_rect(atr_cent_x_last - TORCH_WIDTH_ST, atr_cent_y_last - TORCH_WIDTH_ST, TORCH_WIDTH, 1, PAL_DRK);
+        if (move_dir & J_UP) {
+            fill_bkg_rect(TM_CLAMP_W(atr_cent_x_last - TORCH_WIDTH_ST),
+                          TM_CLAMP_H(atr_cent_y_last + TORCH_HEIGHT_ST), TORCH_WIDTH, 1, PAL_DRK);
         }
-        else if (atr_cent_y < atr_cent_y_last) { // Moving Up
-            fill_bkg_rect(atr_cent_x_last - TORCH_WIDTH_ST, atr_cent_y_last + TORCH_WIDTH_ST, TORCH_WIDTH, 1, PAL_DRK);
-        }            
+        else if (move_dir & J_DOWN) {
+            fill_bkg_rect(TM_CLAMP_W(atr_cent_x_last - TORCH_WIDTH_ST),
+                          TM_CLAMP_H(atr_cent_y_last - TORCH_HEIGHT_ST), TORCH_WIDTH, 1, PAL_DRK);
+        }
 
-        
+
         // Draw edge of new position
-        set_bkg_tiles(atr_cent_x - TORCH_WIDTH_ST, atr_cent_y - TORCH_HEIGHT_ST, 
+        set_bkg_tiles(TM_CLAMP_W(atr_cent_x - TORCH_WIDTH_ST),
+                      TM_CLAMP_H(atr_cent_y - TORCH_HEIGHT_ST),
                       TORCH_WIDTH, TORCH_HEIGHT, torch_attrib_overlay_map);
 
         VBK_REG = VBK_TILES;
     }
-    
+
     atr_cent_x_last = atr_cent_x;
     atr_cent_y_last = atr_cent_y;
 }
 
 
 void init_gfx(void) {
-	
+
 	SPRITES_8x16;
     // Align background to a decent starting point (sprite not in a wall)
 	SCX_REG = 115u;
 	SCY_REG = 110u;
-	
+
     // Load background tiles and map
     set_bkg_data(0, dungeon_TILE_COUNT, dungeon_tiles);
     VBK_REG = VBK_ATTRIBUTES;
     set_bkg_tiles(0, 0, dungeon_WIDTH >> 3, dungeon_HEIGHT >> 3, dungeon_map_attributes);
     VBK_REG = VBK_TILES;
-    set_bkg_tiles(0, 0, dungeon_WIDTH >> 3, dungeon_HEIGHT >> 3, dungeon_map);    
+    set_bkg_tiles(0, 0, dungeon_WIDTH >> 3, dungeon_HEIGHT >> 3, dungeon_map);
 
     set_bkg_palette(0, dungeon_PALETTE_COUNT, dungeon_palettes);
 
     redraw_attribute_map_torch_effect();
-  
+
     // Load sprite tiles and set on center of screen
     SPRITES_8x16;
     set_sprite_data(0, sprite_TILE_COUNT, sprite_tiles);
@@ -163,26 +173,28 @@ void main(void)
     while(1) {
 
         UPDATE_KEYS(); // Read Joypad
+        uint8_t move_dir = keys & (J_LEFT | J_RIGHT | J_UP | J_DOWN);
 
         // Move player (via background scroll) if needed
-        if (keys & J_LEFT) {
+        if (move_dir & J_LEFT) {
             scroll_bkg(-1,0);
             update_player_sprite(PLY_DIR_LEFT);
         }
-        else if (keys & J_RIGHT) {
+        else if (move_dir & J_RIGHT) {
             scroll_bkg(1,0);
             update_player_sprite(PLY_DIR_RIGHT);
         }
-        else if (keys & J_UP) {
+
+        if (move_dir & J_UP) {
             scroll_bkg(0,-1);
             update_player_sprite(PLY_DIR_UP);
         }
-        else if (keys & J_DOWN) {
+        else if (move_dir & J_DOWN) {
             scroll_bkg(0,1);
             update_player_sprite(PLY_DIR_DOWN);
         }
- 
-        update_attribute_map_torch_effect();
+
+        update_attribute_map_torch_effect(move_dir);
         wait_vbl_done(); // Yield CPU till the end of each frame
     }
 }
